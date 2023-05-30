fc6e99ee072f8c2acd96d301b871bb7be91a907e
Colin Vaughn <Colinvaughn01@gmail.com>
2023-05-30 06:42:53 -0400

    Includes Aquas mixin for lowering monuments.

---
+++ src/main/java/org/varietyworldgen/mixin/MonumentBuildingMixin.java
@@ -0,0 +1,18 @@
+package org.varietyworldgen.mixin;
+
+import net.minecraft.structure.OceanMonumentGenerator;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(OceanMonumentGenerator.Base.class)
+public abstract class MonumentBuildingMixin {
+
+    @ModifyConstant(
+            method = "<init>(Lnet/minecraft/util/math/random/Random;IILnet/minecraft/util/math/Direction;)V",
+            constant = @Constant(intValue = 39, ordinal = 0)
+    )
+    private static int mixin(int in) {
+        return 10;
+    }
+}
