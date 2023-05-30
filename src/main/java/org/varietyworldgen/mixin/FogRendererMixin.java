39dcc5d04d7ef8edb0b5ca837c4d35fd982fcadd
Colin Vaughn <Colinvaughn01@gmail.com>
2023-05-30 06:59:11 -0400

    Simplifies the FogRendererMixin so it uses way less calls.

--- src/main/java/org/varietyworldgen/mixin/FogRendererMixin.java
+++ src/main/java/org/varietyworldgen/mixin/FogRendererMixin.java
@@ -8,8 +8,6 @@ import net.minecraft.client.render.Camera;
 import net.minecraft.client.render.CameraSubmersionType;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.tag.BiomeTags;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.registry.RegistryEntry;
@@ -19,7 +17,6 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.varietyworldgen.Util.MathUtil;
 import org.varietyworldgen.Util.RenderSystemUtil;
 import org.varietyworldgen.Varietyworldgen;
 
@@ -29,38 +26,28 @@ public class FogRendererMixin {
     private static void afterSetupFog(Camera camera, BackgroundRenderer.FogType fogType, float viewDistance, boolean thickFog, float tickDelta, CallbackInfo ci) {
         CameraSubmersionType submersionType = camera.getSubmersionType();
         Entity entity = camera.getFocusedEntity();
-        boolean mobEffect = entity instanceof LivingEntity && (((LivingEntity) entity).hasStatusEffect(StatusEffects.BLINDNESS) || ((LivingEntity) entity).hasStatusEffect(StatusEffects.DARKNESS));
 
         if (submersionType == CameraSubmersionType.WATER && Varietyworldgen.config.waterToggle) {
-            overrideWaterFog(viewDistance, entity);
-        }
-    }
+            float fogStart = viewDistance * Varietyworldgen.config.waterStart * 0.01f;
+            float fogEnd = viewDistance * Varietyworldgen.config.waterEnd * 0.01f;
 
-    private static void overrideWaterFog(float viewDistance, Entity entity) {
-        float fogStart = viewDistance * Varietyworldgen.config.waterStart * 0.01f;
-        float fogEnd = viewDistance * Varietyworldgen.config.waterEnd * 0.01f;
+            if (entity instanceof ClientPlayerEntity) {
+                ClientPlayerEntity localPlayer = (ClientPlayerEntity) entity;
+                RegistryEntry<Biome> biomeHolder = localPlayer.world.getBiome(localPlayer.getBlockPos());
 
-        if (entity instanceof ClientPlayerEntity) {
-            ClientPlayerEntity localPlayer = (ClientPlayerEntity) entity;
-            RegistryEntry<Biome> biomeHolder = localPlayer.world.getBiome(localPlayer.getBlockPos());
+                if (biomeHolder.isIn(BiomeTags.HAS_CLOSER_WATER_FOG)) {
+                    fogEnd = viewDistance * Varietyworldgen.config.waterEndSwamp * 0.01f;
+                }
 
-            if (biomeHolder.isIn(BiomeTags.HAS_CLOSER_WATER_FOG)) {
-                fogEnd = viewDistance * Varietyworldgen.config.waterEndSwamp * 0.01f;
+                fogEnd *= Math.max(0.25f, localPlayer.getUnderwaterVisibility());
             }
 
-            fogEnd *= Math.max(0.25f, localPlayer.getUnderwaterVisibility());
+            RenderSystem.setShaderFogStart(fogStart);
+            RenderSystem.setShaderFogEnd(fogEnd);
+            RenderSystemUtil.setShaderFogColor(new Vec3d(0.211, 0.211, 0.211));
+            MinecraftClient.getInstance().worldRenderer.renderLightSky();
+            MinecraftClient.getInstance().worldRenderer.renderDarkSky();
         }
-
-        RenderSystem.setShaderFogStart(fogStart);
-        RenderSystem.setShaderFogEnd(fogEnd);
-        RenderSystemUtil.setShaderFogColor(new Vec3d(0.211, 0.211, 0.211));
-        MinecraftClient.getInstance().worldRenderer.renderLightSky();
-        MinecraftClient.getInstance().worldRenderer.renderDarkSky();
-    }
-
-    private static float vanillaFogStart(float viewDistance) {
-        float f = MathUtil.clamp(viewDistance / 10.0f, 4.0f, 64.0f);
-        return viewDistance - f;
     }
 
     @Shadow
@@ -85,4 +72,3 @@ public class FogRendererMixin {
         }
     }
 }
-
